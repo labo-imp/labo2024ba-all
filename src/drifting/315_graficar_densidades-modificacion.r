588c6c3a017007d21944f623249c1fd9f8bb2de1
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-01 15:35:27 +0000

    auto commit 22       20240901 153512

--- src/drifting/315_graficar_densidades-modificacion.r
+++ src/drifting/315_graficar_densidades-modificacion.r
@@ -1,5 +1,5 @@
-# Script para encontrar visualmente el data drifting
-# focalizado solo en los campos de un buen árbol de decisión
+# Script para encontrar Visuamente  el data drifting
+# focalizado solo en los campos de un buen arbol de deicision
 
 # limpio la memoria
 rm(list = ls()) # remove all objects
@@ -14,62 +14,57 @@ kmes1 <- 202109
 
 #------------------------------------------------------------------------------
 
-graficar_campo <- function(campo, ancho_banda) {
-  # quito de gráfico las colas del 5% de las densidades
+graficar_campo <- function(campo) {
+  # quito de grafico las colas del 5% de las densidades
   qA <- quantile(dataset[foto_mes == kmes0, get(campo)],
-                 prob = c(0.05, 0.95), na.rm = TRUE
+    prob = c(0.05, 0.95), na.rm = TRUE
   )
-  
+
   qB <- quantile(dataset[foto_mes == kmes1, get(campo)],
-                 prob = c(0.05, 0.95), na.rm = TRUE
+    prob = c(0.05, 0.95), na.rm = TRUE
   )
-  
+
   xxmin <- pmin(qA[[1]], qB[[1]])
   xxmax <- pmax(qA[[2]], qB[[2]])
-  
+
   densidad_A <- density(dataset[foto_mes == kmes0, get(campo)],
-                        kernel = "gaussian", bw = ancho_banda, na.rm = TRUE
+    kernel = "gaussian", na.rm = TRUE
   )
-  
+
   densidad_B <- density(dataset[foto_mes == kmes1, get(campo)],
-                        kernel = "gaussian", bw = ancho_banda, na.rm = TRUE
+    kernel = "gaussian", na.rm = TRUE
   )
-  
+
   plot(densidad_A,
-       col = "blue",
-       xlim = c(xxmin, xxmax),
-       ylim = c(0, pmax(max(densidad_A$y), max(densidad_B$y))),
-       main = campo
+    col = "blue",
+    xlim = c(xxmin, xxmax),
+    ylim = c(0, pmax(max(densidad_A$y), max(densidad_B$y))),
+    main = campo
   )
-  
+
   lines(densidad_B, col = "red", lty = 2)
-  
+
   legend("topright",
-         legend = c(kmes0, kmes1),
-         col = c("blue", "red"), lty = c(1, 2)
+    legend = c( kmes0, kmes1),
+    col = c("blue", "red"), lty = c(1, 2)
   )
 }
-
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-# Aquí comienza el programa
+# Aqui comienza el programa
 setwd("~/buckets/b1/") # Establezco el Working Directory
 
-# cargo miAmbiente
-miAmbiente <- read_yaml("~/buckets/b1/miAmbiente.yml")
+#cargo miAmbiente
+miAmbiente <- read_yaml( "~/buckets/b1/miAmbiente.yml" )
 
 # cargo dataset
-dataset <- fread(miAmbiente$dataset_pequeno)
+dataset <- fread( miAmbiente$dataset_pequeno )
 
 dir.create("./exp/", showWarnings = FALSE)
 dir.create("./exp/DR3150/", showWarnings = FALSE)
 setwd("./exp/DR3150/")
 
 dataset <- dataset[foto_mes %in% c(kmes0, kmes1)]
-
-# calculo el ancho de banda basado en ambos meses
-datos_combined <- dataset[foto_mes %in% c(kmes0, kmes1), get(campo)]
-ancho_banda <- density(datos_combined, kernel = "gaussian")$bw
 
 # creo la clase_binaria SI={ BAJA+1, BAJA+2 }    NO={ CONTINUA }
 dataset[
@@ -78,6 +73,8 @@ dataset[
 ]
 
 # Entreno el modelo
+# utilizo los mejores hiperparametros encontrados
+# en una Bayesian Optimizationcon 5-fold Cross Validation
 modelo <- rpart(
   formula = "clase_binaria ~ . -clase_ternaria",
   data = dataset[foto_mes == kmes0], # los datos donde voy a entrenar
@@ -88,6 +85,7 @@ modelo <- rpart(
   maxdepth = 8
 )
 
+
 campos_modelo <- names(modelo$variable.importance)
 campos_buenos <- c(campos_modelo, setdiff(colnames(dataset), campos_modelo))
 campos_buenos <- setdiff(
@@ -95,16 +93,16 @@ campos_buenos <- setdiff(
   c("foto_mes", "clase_ternaria", "clase_binaria")
 )
 
-# genero los gráficos en un archivo
-pdf(paste0("densidades_mismo_bw_kernel_", kmes0, "_", kmes1, ".pdf"))
+
+# genero los graficos en un archivo
+pdf( paste0("densidades_", kmes0, "_", kmes1, ".pdf") )
 
 for (campo in campos_buenos) {
   cat(campo, "  ")
-  graficar_campo(campo, ancho_banda)
+  graficar_campo(campo)
 }
 
 dev.off()
-
 
 
 # copio al bucket para Modalidad Conceptual
