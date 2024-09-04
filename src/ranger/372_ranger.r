38a52bf76005eb8767c6da15b80afc7d62eb3825
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-04 12:04:28 +0000

    auto commit 12       20240904 120411

--- src/ranger/372_ranger.r
+++ src/ranger/372_ranger.r
@@ -16,10 +16,10 @@ PARAM$experimento <- 3720
 
 # hiperparámetros de Random Forest
 PARAM$ranger <- list(
-  "num.trees" = 300, # cantidad de arboles
-  "mtry" = 7, # cantidad de atributos que participan en cada split
-  "min.node.size" = 5, # tamaño minimo de las hojas
-  "max.depth" = 15 # 0 significa profundidad infinita
+  "num.trees" = 500, # cantidad de arboles
+  "mtry" = 6, # cantidad de atributos que participan en cada split
+  "min.node.size" = 869, # tamaño minimo de las hojas
+  "max.depth" = 20 # 0 significa profundidad infinita
 )
 
 #------------------------------------------------------------------------------
