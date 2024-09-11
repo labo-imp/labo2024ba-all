3bdc22bce993610d42303cfeb8d9afb08c3b99cd
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-11 15:04:38 +0000

    auto commit 14       20240911 150423

--- src/rpart/222_traintest_montecarlo.r
+++ src/rpart/222_traintest_montecarlo.r
@@ -10,8 +10,8 @@ PARAM <- list()
 PARAM$semillas <- c(100019, 100043,100103, 100127, 100183)
 
 # elegir SU dataset comentando/ descomentando
-#PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
-PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
+PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
+#PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
 
 PARAM$training_pct <- 70L  # entre  1L y 99L 
 
