a441e5cf9cc8f88fc67a9a78ba93145b88189253
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-21 15:16:04 +0000

    auto commit 13       20240921 151548

--- src/workflows/B519_workflow_base_f202109_conc.r
+++ src/workflows/B519_workflow_base_f202109_conc.r
@@ -272,11 +272,12 @@ TS_strategy_base9 <- function( pinputexps )
 
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202107, 202106, 202105, 202104, 202103, 202102, 202101, 202012 )
+  param_local$final_train$training <- c(202104, 202103, 202102, 202101, 202012, 202011,
+    202010, 202009)
 
 
-  param_local$train$training <- c(202104, 202103, 202102, 202101,
-    202012 ,202011, 202010, 202009)
+  param_local$train$training <- c(202107, 202106, 202105, 202104, 202103, 202102, 202101,
+    202012 )
   param_local$train$validation <- c(202106 , 202105)
   param_local$train$testing <- c(202107)
 
