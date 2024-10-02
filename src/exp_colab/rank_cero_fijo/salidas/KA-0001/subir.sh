f1dc25b1fdedf5acb9aa5304824027ba99bcccb4
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-02 19:02:02 +0000

    auto commit 16       20241002 190136

---
+++ src/exp_colab/rank_cero_fijo/salidas/KA-0001/subir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+source ~/.venv/bin/activate  
+kaggle competitions submit -c labo-i-vivencial-2024-ba -f KA-0001_01_052_s819563_13000.csv -m "KA-0001 , KA-0001_01_052_s819563_13000.csv"
+deactivate 
