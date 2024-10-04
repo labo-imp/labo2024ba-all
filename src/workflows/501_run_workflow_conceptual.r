2df159ec47a4aaf2b18f2a0310c5441e1a3ead62
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-04 22:01:06 +0000

    auto commit 35       20241004 220049

--- src/workflows/501_run_workflow_conceptual.r
+++ src/workflows/501_run_workflow_conceptual.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/517_workflow_base_f202107_conc.r"
+PARAM <- "src/workflows/e517_workflow_base_f202107_conc.r"
 
 envg <- env()
 
--- src/workflows/z501_run_workflow_conceptual.r
+++ src/workflows/z501_run_workflow_conceptual.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/z519_workflow_base_f202109_conc.r"
+PARAM <- "src/workflows/c517_workflow_base_f202107_conc.r"
 
 envg <- env()
 
