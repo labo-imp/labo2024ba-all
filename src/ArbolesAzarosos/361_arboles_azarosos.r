b19e2a321c4f5fe3d7a606069a88fe705893bb55
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-13 23:36:45 +0000

    auto commit 4       20241013 233630

--- src/ArbolesAzarosos/361_arboles_azarosos.r
+++ src/ArbolesAzarosos/361_arboles_azarosos.r
@@ -15,16 +15,16 @@ require("yaml")
 
 # parametros experimento
 PARAM <- list()
-PARAM$experimento <- 3610
+PARAM$experimento <- 3611
 
 # parametros rpart
 
 #  cargue aqui los hiperparametros elegidos
 PARAM$rpart <- data.table( 
   "cp" = -1,
-  "minsplit" = 50,
-  "minbucket" = 20,
-  "maxdepth" = 10
+  "minsplit" = 30,
+  "minbucket" = 5,
+  "maxdepth" = 6
 )
 
 # parametros  arbol
--- src/ranger/372_ranger.r
+++ src/ranger/372_ranger.r
@@ -16,10 +16,10 @@ PARAM$experimento <- 3720
 
 # hiperparámetros de Random Forest
 PARAM$ranger <- list(
-  "num.trees" = 1000, # cantidad de arboles
-  "mtry" = 3, # cantidad de atributos que participan en cada split
-  "min.node.size" = 5, # tamaño minimo de las hojas
-  "max.depth" = 20 # 0 significa profundidad infinita
+  "num.trees" = 500, # cantidad de arboles
+  "mtry" = 2, # cantidad de atributos que participan en cada split
+  "min.node.size" = 217, # tamaño minimo de las hojas
+  "max.depth" = 25 # 0 significa profundidad infinita
 )
 
 #------------------------------------------------------------------------------
