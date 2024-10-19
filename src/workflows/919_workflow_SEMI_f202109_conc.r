43cf4937f51c2e83c86d0afb3a9a16d6d56920a5
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-19 11:28:23 +0000

    auto commit 61       20241019 112810

--- src/workflows/919_workflow_SEMI_f202109_conc.r
+++ src/workflows/919_workflow_SEMI_f202109_conc.r
@@ -348,8 +348,8 @@ HT_tuning_semillerio <- function( pinputexps, semillerio, bo_iteraciones, bypass
     max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
     min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
     min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
-    lambda_l1 = 0.0, # lambda_l1 >= 0.0
-    lambda_l2 = 0.0, # lambda_l2 >= 0.0
+   #lambda_l1 = 0.0, # lambda_l1 >= 0.0
+   #lambda_l2 = 0.0, # lambda_l2 >= 0.0
     max_bin = 31L, # lo debo dejar fijo, no participa de la BO
     early_stopping = 0,  # No se hace early stopping
 
@@ -365,10 +365,13 @@ HT_tuning_semillerio <- function( pinputexps, semillerio, bo_iteraciones, bypass
 
     extra_trees = FALSE,
     # Parte variable
-    learning_rate = c( 0.02, 0.3 ), ## Corrida original
-    feature_fraction = c( 0.05, 0.99),
-    num_leaves = c( 8L, 8196L,  "integer" ), 
-    min_data_in_leaf = c( 5L, 50000L, "integer" ) 
+    learning_rate = c( 0.2, 1.2 ),
+    feature_fraction = c( 0.01, 0.9 ),
+    lambda_l1 = c(1.0, 1000.0), ##AA 19010
+    lambda_l2 = c(1.0, 1000.0), ##AA 19010
+    num_iterations_log = c(2, 8),  # directo a num_iterations 2^ 
+    leaf_size_log = c( -11, -5),   # deriva en min_data_in_leaf
+    coverage_log = c( -4, 0 )      # deriva en num_leaves
   )
 
 
